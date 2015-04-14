2f60a283afe16d5a2b2b2005bfabdeba2539d709
free-info <irth03@gmail.com>
2015-04-13 21:05:12 -0700

    Initial commit

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('adolfControllers', [])
+
+.controller('adolfCtrl', function($scope, adolfService) {
+		$scope.wordListObject = adolfService.data.wordListObject;
+		$scope.wordListArray = adolfService.data.wordListArray;
+		$scope.textObj = adolfService.data.textObj;
+		$scope.wordSave = adolfService.api.wordSave;
+		$scope.remove = adolfService.api.remove;
+		$scope.update = adolfService.api.update;
+		$scope.new = adolfService.models.newWord;
+		$scope.wordListObject.$bindTo($scope,'data');
+		$scope.textObj.$bindTo($scope,'text');
+	});
---
+++ platforms/ios/www/js/services.js
@@ -0,0 +1,35 @@
+angular.module('adolfServices', ['firebase'])
+
+.factory('adolfService', function($firebaseObject, $firebaseArray) {
+		// data structure
+		var service = {data:{},api:{},models:{}};
+
+		// gather and prepare data
+		var wordList = new Firebase('http://adolf.firebaseio.com/wordList');
+		var text = new Firebase('http://adolf.firebaseio.com/text');
+		var obj = $firebaseObject(wordList);
+		var arr = $firebaseArray(wordList);
+		var textObj = $firebaseObject(text);
+		service.data.wordListObject = obj;
+		service.data.wordListArray = arr;
+		service.data.textObj = textObj;
+
+		// define database methods
+		service.api.wordSave = function(wordEntry){
+			service.data.wordListArray.$add(wordEntry);
+		};
+		service.api.remove = function(id, arr){
+			console.log(arr ,id);
+			arr.$remove(id);
+		}
+		service.api.update = function(obj, val){
+			console.log(val,obj)
+			obj.$save();
+		}
+
+		// define data model for new words
+		service.models.newWord={name:'', meanings:[{wz0:''},{wz1:''}]};
+
+		// object to be injected in controller
+		return service;
+});
